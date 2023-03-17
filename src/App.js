41341d79828459140aa5d3d8cbf7db4038e59ea2
jaeyeoneej <5ikve@naver.com>
2023-03-17 17:42:30 +0900

    mergy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
